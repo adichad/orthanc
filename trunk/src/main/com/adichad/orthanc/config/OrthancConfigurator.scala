447a374205be787cce8f632760311fb192edfc8b
Aditya Varun Chadha <adi@adichad.com>
2012-08-14 16:04:08 +0530

    license headers added

--- trunk/src/main/com/adichad/orthanc/config/OrthancConfigurator.scala
+++ trunk/src/main/com/adichad/orthanc/config/OrthancConfigurator.scala
@@ -1,3 +1,20 @@
+/**
+ * OrthancConfigurator.scala, part of Orthanc
+ * Copyright 2012 Aditya Varun Chadha ( adi@adichad.com )
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.adichad.orthanc.config
 
 import java.util.Properties
